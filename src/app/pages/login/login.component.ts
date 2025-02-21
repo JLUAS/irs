5432c6b700ad1671cf948f58f698d73a04af6c0b
JLUAS <jlurquieta1@gmail.com>
2025-02-21 16:38:13 -0600

    Configuracion google

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 declare const google: any; // Declara el objeto global
 
@@ -9,37 +10,43 @@ declare const google: any; // Declara el objeto global
 })
 export class LoginComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
   ngOnInit(): void {
-    // Inicializa el objeto de Google Sign-In usando la configuración de la documentación oficial
+    // Inicializa el objeto de Google Sign-In
+    if(localStorage.getItem('token-irs')){
+      this.router.navigate(['/home']);
+    }
     google.accounts.id.initialize({
       client_id: '342953985229-r32f674o0sk5e1kd7e0jdslnt2l0033d.apps.googleusercontent.com', // Reemplaza con tu Client ID
       callback: this.handleCredentialResponse.bind(this),
-      auto_select: false, // Opcional: para evitar autoselección de cuentas
+      auto_select: false,
     });
 
-    // Renderiza el botón de Google Sign-In en el div con id "googleSignInDiv"
+    // Renderiza el botón de Google Sign-In
     google.accounts.id.renderButton(
       document.getElementById('googleSignInDiv'),
-      { theme: 'outline', size: 'large' } // Configura el estilo según la documentación
+      { theme: 'outline', size: 'large' }
     );
   }
 
-  // Función que maneja la respuesta de Google una vez autenticado el usuario
+  // Función que maneja la respuesta de autenticación de Google
   handleCredentialResponse(response: any): void {
     const token = response.credential;
-    localStorage.setItem('token', token);
+    localStorage.setItem('token-irs', token);
 
-    // Decodifica el token para obtener la información del usuario (en un entorno real, haz la validación en el backend)
+    // Decodifica el token para extraer la información del usuario
     const payload = JSON.parse(atob(token.split('.')[1]));
     const email = payload.email;
 
-    // Aquí puedes agregar la lógica para aceptar solo correos institucionales, por ejemplo:
+    // Valida que el correo pertenezca al dominio institucional
     if (email.endsWith('@tec.mx')) {
       console.log('Usuario autorizado:', email);
-      // Procede a redirigir o almacenar el estado de la sesión
+      // Redirige a la ruta home
+      this.router.navigate(['/home']);
     } else {
       alert('Acceso restringido solo a alumnos del Tec de Monterrey.');
-      // Puedes revocar el acceso o limpiar el token
+      localStorage.removeItem('token');
     }
   }
 }
