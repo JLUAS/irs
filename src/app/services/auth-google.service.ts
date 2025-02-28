eabffbceee5f9f048cb06309b70708778f6e0382
JLUAS <jlurquieta1@gmail.com>
2025-02-28 14:28:30 -0600

    try

--- src/app/services/auth-google.service.ts
+++ src/app/services/auth-google.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Route, Router } from '@angular/router';
-import { AuthConfig, OAuthService } from 'angular-oauth2-oidc'
+import { Router } from '@angular/router';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 
 @Injectable({
   providedIn: 'root'
@@ -18,17 +18,28 @@ export class AuthGoogleService {
       clientId: '342953985229-r32f674o0sk5e1kd7e0jdslnt2l0033d.apps.googleusercontent.com',
       redirectUri: 'https://irs-kohl.vercel.app/main',
       scope: 'openid profile email',
-    }
+    };
 
     this.oauthService.configure(config);
     this.oauthService.setupAutomaticSilentRefresh();
+
+    // Subscribe to token_received event to set the token in localStorage
+    this.oauthService.events.subscribe(event => {
+      if (event.type === 'token_received') {
+        const token = this.oauthService.getAccessToken();
+        localStorage.setItem('token-irs', token);
+        console.log('Token set in localStorage:', token);
+        // Optionally, you can navigate after token is received
+        this.router.navigate(['/main']);
+      }
+    });
+
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
-    this.router.navigate(["/main"])
   }
 
   isAuthenticated(): boolean {
     const token = localStorage.getItem('token-irs');
-    console.log(!!token)
+    console.log(!!token);
     return !!token;
   }
 
@@ -38,10 +49,10 @@ export class AuthGoogleService {
 
   logout() {
     this.oauthService.logOut();
+    localStorage.removeItem('token-irs');
   }
 
   getProfile() {
     return this.oauthService.getIdentityClaims();
   }
-
 }
